8b924d0cee2d77fe3b8c7238de10f8a009406c43
mrtnRitter <mrtn.ritter@googlemail.com>
2024-11-21 22:37:34 +0100

    init

---
+++ res.py
@@ -0,0 +1,26 @@
+struct_cpn = {
+    "437" : "United States",
+    "850" : "Multilingual",
+    "860" : "Portugal",
+    "863" : "Canada-French",
+    "865" : "Nordic"
+    }
+
+struct_dfc = {
+    "STL25.01" : "25fps",
+    "STL30.01" : "30fps"
+    }
+
+struct_dsc = ["Open subtitling","Level-1 teletext","Level-2 teletext"]
+
+struct_cct = ["Latin","Cyrillic","Arabic","Greek","Hebrew"]
+
+struct_lc = [
+    "Unknown","Albanian","Breton","Catalan","Croatian","Welsh","Czech","Danish","German","English","Spanish","Esperanto","Estonian","Basque","Faroese","French","Frisian","Irish","Gaelic","Gaelic","Icelandic","Italian",
+    "Lappish","Latin","Latvian","Luxembourgian","Lithuanian","Hungarian","Maltese","Dutch","Norwegian","Occitan","Polish","Portugese","Romanian","Romansh","Serbian","Slovak","Slovenian","Finnish","Swedish","Turkish","Flemish","Wallon",
+    "","","","","","","","","","","","","","","","","","","","","","","","","","Zulu","Vietnamese","Uzbek","Urdu","Ukrainian","Thai","Telugu","Tatar","Tamil","Tadzhik","Swahili","Sranan Tongo","Somali","Sinhalese","Shona",
+    "Serbo-croat","Ruthenian","Russian","Quechua","Pushtu","Punjabi","Persian","Papamiento","Oriya","Nepali","Ndebele","Marathi","Moldavian","Malaysian","Malagasay","Macedonian","Laotian","Korean","Khmer","Kazakh","Kannada","Japanese",
+    "Indonesian","Hindi","Hebrew","Hausa","Gurani","Gujurati","Greek","Georgian","Fulani","Dari","Churash","Chinese","Burmese","Bulgarian","Bengali","Bielorussian","Bambora","Azerbaijani","Assamese","Armenian","Arabic","Amharic"
+    ]
+
+struct_tcs = ["Not intended for use", "Intended for use"]
---
+++ stl-scope.py
@@ -0,0 +1,132 @@
+# ------------- IMPORTS ------------- #
+import res
+
+
+# ------------- GLOBALS ------------- #
+
+gsi_readmap = [3,8,1,2,2,32,32,32,32,32,32,16,6,6,2,5,5,3,2,2,1,8,8,1,1,3,32,32,32,75,576]
+tti_readmap = [1,2,1,1,4,4,1,1,1,112]
+
+gsi_block = {
+    "Code Page Number" : "",
+    "Disk Format Code" : "",
+    "Display Standard Code" : "",
+    "Character Code Table" : "",
+    "Language Code" : "",
+    "Original Progamme Title" : "",
+    "Original Episode Title" : "",
+    "Translated Programme Title" : "",
+    "Translated Episode Title" : "",
+    "Translator's Name" : "",
+    "Translator's Contact Details" : "",
+    "Subtitle List Reference Code" : "",
+    "Creation Date" : "",
+    "Revision Date" : "",
+    "Revision number" : "",
+    "Total Number of TTI blocks" : "",
+    "Total Number of subtitles" : "",
+    "Total Number of subtitle groups" : "",
+    "Max Characters in line" : "",
+    "Max lines" : "",
+    "TC Status" : "",
+    "TC Start" : "",
+    "TC First In-Cue" : "",
+    "Total Number of Disks" : "",
+    "Disk Number" : "",
+    "Country of Origin" : "",
+    "Publisher" : "",
+    "Editor's Name" : "",
+    "Editor's Contact Details" : "",
+    "Spare Bytes" : "",
+    "User Defined" : ""
+    }
+
+
+tti_block = {
+    "Subtitle Group Number" : "",
+    "Subtitle Number" : "",
+    "Extension Block Number" : "",
+    "Cumulative Status" : "",
+    "Time Code In" : "",
+    "Time Code Out" : "",
+    "Vertical Position" : "",
+    "Justification Code" : "",
+    "Comment Flag" : "",
+    "Text Field" : ""
+}
+
+tti_blocks = []
+
+
+# ------------- FUNCTIONS ------------- #
+
+with open ("Testfile/test.stl", "rb") as file:
+    for byte, key in enumerate(gsi_block):
+        val = file.read(gsi_readmap[byte]).decode("utf8")
+        
+        if val.isspace():
+            val = "Undefined"
+
+        val = val.strip()
+
+        if key == "Code Page Number":
+            val += " " + res.struct_cpn[val]
+
+        if key == "Disk Format Code":
+            val += " " + res.struct_dfc[val]
+
+        if key == "Display Standard Code":
+            if val != "Undefined":
+                val += " " + res.struct_dsc[int(val)]
+
+        if key == "Character Code Table":
+            val += " " + res.struct_cct[int(val)]
+
+        if key == "Language Code":
+            val += " " + res.struct_lc[int(val, 16)]
+
+        if key == "Creation Date" or key == "Revision Date":
+            val += " " + val[4:6] + "." + val[2:4] + "." + val[0:2]
+
+        if key == "Revision number" or key == "Total Number of TTI blocks" or key == "Total Number of subtitles" or key == "Total Number of subtitle groups":
+            val = str(int(val))
+        
+        if key == "Total Number of TTI blocks":
+            val = int(val)
+
+        if key == "TC Status":
+            val += " " + res.struct_tcs[int(val)]
+
+        if key == "TC Start" or key == "TC First In-Cue":
+            val = val[0:2] + ":" + val[2:4] + ":" + val[4:6] + ":" + val[6:8] 
+
+        gsi_block[key] = val
+
+    for key in gsi_block:
+        print(key + " : " + str(gsi_block[key]))
+
+    print("--------------------------------------------------")
+
+    for tti in range(gsi_block["Total Number of TTI blocks"]):
+        for byte, key in enumerate(tti_block):
+            val = file.read(tti_readmap[byte]).hex()
+
+            if key == "Subtitle Group Number":
+                val = int(val, 16)
+
+            if key == "Subtitle Number":
+                val = str(int(val[2:4], 16) + int(val[0:2], 16) + 1)
+
+            if key == "Time Code In" or key == "Time Code Out":
+                val = str(int(val[0:2], 16)).zfill(2) + ":" + str(int(val[2:4], 16)).zfill(2) + ":" + str(int(val[4:6], 16)).zfill(2) + ":" + str(int(val[6:8], 16)).zfill(2)
+
+            if key == "Vertical Position":
+                val = int(val, 16)
+
+            tti_block[key] = val
+
+        tti_blocks.append(tti_block.copy())
+
+    for tti_block in tti_blocks:
+        print(tti_block)
+
